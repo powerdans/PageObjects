977922c19bb18f913a5409afa9e4c8802d40286b
Vereshchagin <danilv-00@mail.ru>
2023-11-15 18:11:48 +0300

    aded

--- src/test/java/pacage/companents/RezultComponent.java
+++ src/test/java/pacage/companents/RezultComponent.java
@@ -1,4 +1,11 @@
 package pacage.companents;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 public class RezultComponent {
+    public void checkTableResult(String key, String value) {
+        $(".table-responsive").$(byText(key)).parent()
+                .shouldHave(text(value));
+    }
 }
